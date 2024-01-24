907cd6d619ac69815326292cd99b8fc59b3ee3ba
Oriol Riera Lizcano <152235615+Uriera4@users.noreply.github.com>
2024-01-24 11:56:59 +0100

    "S2.03.EstructuraDeDadesMongoDB"

--- n1exercici1/Clients.js
+++ n1exercici1/Clients.js
@@ -1,8 +1,8 @@
 
-var mongoose = require('mongoose');var Schema = mongoose.Schema;var Clients = new Schema({IDClient: {type: Schema.Types.ObjectId, required: true},Nom: {type: String, required: true},Informacio: {
-Adreça: {
-Carrer: {type: String, required: true},Numero: {type: Number, required: true},Pis: {type: String},Porta: {type: String},Ciutat: {type: String, required: true},Codi Postal: {type: String, required: true},Pais: {type: String, required: true}},Telefon: {type: Number, required: true},Correu Electronic: {type: String, required: true},Data Registre: {type: Date, required: true}},Compres: [{
-IDCompra: {type: Schema.Types.ObjectId, required: true},Ulleres: {
-IDUlleres: {type: Schema.Types.ObjectId, required: true},Marca: {type: String, required: true},Graduacio: {
-Esquerra: {type: Number, required: true},Dreta: {type: Number, required: true}},Color Vidre: {
-Esquerra: {type: String},Dreta: {type: String}},Montura: {type: String, required: true},Preu: {type: Number, required: true}}}]}); 
+db.createCollection( 'Clients', {validator: {$jsonSchema: {bsonType: 'object',title:'Clients',required: [         'IDClient',          'Nom',          'Informacio',          'Compres'],properties: {IDClient: {bsonType: 'objectId'},Nom: {bsonType: 'string'},Informacio: {bsonType: 'object',
+title:'object',required: [         'Adreça',          'Telefon',          'Correu Electronic',          'Data Registre'],properties: {Adreça: {bsonType: 'object',
+title:'object',required: [         'Carrer',          'Numero',          'Ciutat',          'Codi Postal',          'Pais'],properties: {Carrer: {bsonType: 'string'},Numero: {bsonType: 'int'},Pis: {bsonType: 'string'},Porta: {bsonType: 'string'},Ciutat: {bsonType: 'string'},Codi Postal: {bsonType: 'int'},Pais: {bsonType: 'string'}}},Telefon: {bsonType: 'int'},Correu Electronic: {bsonType: 'string'},Data Registre: {bsonType: 'date'}}},Compres: {bsonType: 'array',items: {
+title:'object',required: [         'IDCompra',          'Ulleres'],properties: {IDCompra: {bsonType: 'objectId'},Ulleres: {bsonType: 'object',
+title:'object',required: [         'IDUlleres',          'Marca',          'Graduacio',          'Color Vidre',          'Montura',          'Preu'],properties: {IDUlleres: {bsonType: 'objectId'},Marca: {bsonType: 'string'},Graduacio: {bsonType: 'object',
+title:'object',required: [         'Esquerra',          'Dreta'],properties: {Esquerra: {bsonType: 'double'},Dreta: {bsonType: 'double'}}},Color Vidre: {bsonType: 'object',
+title:'object',required: [         'Esquerra',          'Dreta'],properties: {Esquerra: {bsonType: 'string'},Dreta: {bsonType: 'string'}}},Montura: {enum: "metal·lica", "pasta", "flotant"},Preu: {bsonType: 'double'}}}}}}}         }      }});  
